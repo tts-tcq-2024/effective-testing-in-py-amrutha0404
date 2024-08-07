96ca6f0d0d35039e7e150986c2350a756a77823f
amrutha0404 <99201771+amrutha0404@users.noreply.github.com>
2024-08-07 15:20:50 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -8,7 +8,7 @@ def size(cms):
         return 'L'
 
 
-assert(size(37) == 'S')
-assert(size(40) == 'M')
+assert(size(38) == 'S')
+assert(size(42) == 'M')
 assert(size(43) == 'L')
 print("All is well (maybe!)\n")
